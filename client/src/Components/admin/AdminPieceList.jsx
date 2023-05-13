d690809959125b0d2370fc203806e2a514720565
talbihend <hendtalbi96@gmail.com>
2023-05-13 18:45:48 +0100

    2

--- client/src/Components/admin/AdminPieceList.jsx
+++ client/src/Components/admin/AdminPieceList.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getPieces } from '../../redux/actions/pieceActions';
 import AdminPieceCard from './AdminPieceCard';
+
 
 const AdminPieceList = () => {
 
   const { pieces, loading } = useSelector((state) => state.piecesReducers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPieces());
+  }, []);
   
 
 
